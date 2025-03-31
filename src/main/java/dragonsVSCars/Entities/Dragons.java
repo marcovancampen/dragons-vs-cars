5673152d9bd447ba136a264d9f30b4fd00140d6a
jopsco01 <83950678+jopsco01@users.noreply.github.com>
2025-03-31 10:38:16 +0200

    damage variabele toegevoegd

--- src/main/java/dragonsVSCars/Entities/Dragons.java
+++ src/main/java/dragonsVSCars/Entities/Dragons.java
@@ -16,12 +16,14 @@ public class Dragons extends DynamicSpriteEntity implements SceneBorderTouchingW
     protected int attackSpeed;
     protected int attackRange;
     protected int upgradeCost;
+    protected int attackDamage;
 
-    protected Dragons(String resource, Coordinate2D initialLocation, int attackSpeed, int attackRange, int upgradeCost) {
+    protected Dragons(String resource, Coordinate2D initialLocation, int attackSpeed, int attackRange, int upgradeCost, int attackDamage) {
         super(resource, initialLocation);
         this.attackSpeed = attackSpeed;
         this.attackRange = attackRange;
         this.upgradeCost = upgradeCost;
+        this.attackDamage = attackDamage;
     }
 
     public void spawnDragon() {
